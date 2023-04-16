f61e6b1d0b3b99e01edd8c5530d3b0ff1589122f
magga <adb.dao@gmail.com>
2023-04-16 20:31:15 +0800

    CacheOnly

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox
 // 存任何的.js
 workbox.routing.registerRoute(
   new RegExp('*\.js'),
-  workbox.strategies.cacheFirst({
+  workbox.strategies.CacheOnly({
     cacheName: 'js-cache'
   })
 );
@@ -13,7 +13,7 @@ workbox.routing.registerRoute(
 // 存任何的html
 workbox.routing.registerRoute(
   new RegExp('*\.html'),
-  workbox.strategies.cacheFirst({
+  workbox.strategies.CacheOnly({
     cacheName: 'html-cache'
   })
 );
@@ -21,7 +21,7 @@ workbox.routing.registerRoute(
 // 存任何的css
 workbox.routing.registerRoute(
   new RegExp('*\.css'),
-  workbox.strategies.cacheFirst({
+  workbox.strategies.CacheOnly({
     cacheName: 'css-cache'
   })
 );
@@ -29,7 +29,7 @@ workbox.routing.registerRoute(
 // 存任何的images
 workbox.routing.registerRoute(
   new RegExp('icons/*\.(?:png|jpg|jpeg|svg|gif)'),
-  workbox.strategies.cacheFirst({
+  workbox.strategies.CacheOnly({
     cacheName: 'image-cache'
   })
 );
@@ -37,7 +37,7 @@ workbox.routing.registerRoute(
 // 存任何的.js
 workbox.routing.registerRoute(
   new RegExp('/pwa_blog/*\.js'),
-  workbox.strategies.cacheFirst({
+  workbox.strategies.CacheOnly({
     cacheName: 'js-cache-github'
   })
 );
@@ -45,7 +45,7 @@ workbox.routing.registerRoute(
 // 存任何的html
 workbox.routing.registerRoute(
   new RegExp('/pwa_blog/*\.html'),
-  workbox.strategies.cacheFirst({
+  workbox.strategies.CacheOnly({
     cacheName: 'html-cache-github'
   })
 );
@@ -53,7 +53,7 @@ workbox.routing.registerRoute(
 // 存任何的css
 workbox.routing.registerRoute(
   new RegExp('/pwa_blog/*\.css'),
-  workbox.strategies.cacheFirst({
+  workbox.strategies.CacheOnly({
     cacheName: 'css-cache-github'
   })
 );
@@ -61,7 +61,7 @@ workbox.routing.registerRoute(
 // 存任何的images
 workbox.routing.registerRoute(
   new RegExp('/pwa_blog/icons/*\.(?:png|jpg|jpeg|svg|gif)'),
-  workbox.strategies.cacheFirst({
+  workbox.strategies.CacheOnly({
     cacheName: 'image-cache-github'
   })
 );
